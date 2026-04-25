5a02229ea8021def02b08ebbf6d23bf9e7e3b063
max normand <maxnormand97@gmail.com>
2026-04-25 11:44:00 +1000

    make implementation notes and plan for robot class - create blank constructor class and instance methods with comments on requirements - write out test cases to fill in later

--- robot.rb
+++ robot.rb
@@ -1,19 +1,38 @@
 # frozen_string_literal: true
 
-# TODO's:
-# build out constructor
-# define initial public methods
-# map out data structures
-# map out edge cases
-# map out test skeletons
-# figure out gem dependency (if needed) for CLI tool (this thing needs to be a REPL)
-# TTY::Prompt - nice for more interactive CLI experience
-# Thor for more granular control
-# OR just the classic Ruby Readline!
+# NOTES:
+# We may actaully want to have a Grid class, because the Grid iteself is a seperate entity
+# to the Robot, we would break the single responsiblity principle if we handled that logic here!
 
 # Creates a robot that will move along a grid of 6 x 6
 class Robot
-  def say_gday
-    puts "I AM ALIVE G'DAY!"
+  # TODO: probably want to define our constants for north
+
+  def initialize
+    # orientation - is the robot facing N, S, E, W
+    # position - where is the robot on the grid now
   end
+
+  # where to place to robot on the grid up / down, left / right and orientation / rotation axis
+  def place(x_axis, y_axis, orientation)
+    # CHECK: can this function only be called once per REPL session? Once the robot is on the Grid
+    # it does not make sense to be able to call this function. Maybe the placement should occur in the
+    # constructor
+
+    # TODO: all other commands are to be ignored until the robot is placed!
+  end
+
+  # moves the robot one step forward in whicher direction it is facing
+  def move
+    # TODO: the robot must stay in the bounds of the grid, this must be handled here
+  end
+
+  # turns robot 90 degrees to the left
+  def left; end
+
+  # turns robot 90 degrees to the right
+  def right; end
+
+  # Outputs where the robot currently is
+  def report; end
 end
--- test/test_robot.rb
+++ test/test_robot.rb
@@ -4,9 +4,35 @@ require 'minitest/autorun'
 require_relative '../robot'
 
 class RobotTest < Minitest::Test
-  # TODO: fill out unit test cases later
-  def test_say_gday
-    robot = Robot.new
-    assert_respond_to robot, :say_gday
+  def setup
+    @robot = Robot.new
+  end
+
+  def test_robot_cannot_move_before_placement
+    # assert that move, left, right, report do nothing before place is called
+  end
+
+  def test_place_sets_position_and_orientation
+    # assert that place sets the correct position and orientation
+  end
+
+  def test_place_ignores_invalid_positions
+    # assert that place ignores positions outside the grid
+  end
+
+  def test_move_within_bounds
+    # assert that move updates position correctly within grid bounds
+  end
+
+  def test_move_ignores_out_of_bounds
+    # assert that move does not update position if it would go off the grid
+  end
+
+  def test_left_and_right_turns
+    # assert that left and right update orientation correctly
+  end
+
+  def test_report_outputs_position_and_orientation
+    # assert that report returns the correct string or output
   end
 end
